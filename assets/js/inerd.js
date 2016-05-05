0b610367f42819f52d2c0808d95431415bfad2e8
onlylemi <xiaomi_0623@163.com>
2016-05-05 13:06:41 +0800

    sub-url is ok

--- assets/js/inerd.js
+++ assets/js/inerd.js
@@ -5,31 +5,51 @@
  * @time: 2016-05-04 23:32
  */
 
-$(document).ready(function(){
+$(document).ready(function() {
 
 
     // 提交 url 地址
-  $("#sub-url-submit").click(function(){
-    var type = $("#sub-url-type").val();
-    var url = $("#sub-url").val();
-    var finder = $("#sub-url-finder").val();
+    $("#sub-url-submit").click(function() {
+        var type = $("#sub-url-type").val();
+        var url = $("#sub-url").val();
+        var finder = $("#sub-url-finder").val();
 
-    /*if (url == ""){
-        alert("请输入干货 URL 地址 ！！！");
-        return;
-    }
+        if (url == "") {
+            alert("\n\n请输入干货 URL 地址 ！！！\n\n");
+            return;
+        }
 
-    if (finder == ""){
-        alert("请输入您的大名，让我们知道您 ！！！");
-        return;
-    }*/
+        if (!isURL(url)) {
+            alert("\n\n别捣乱啊，输入正确的 URL 地址 ！！！\n\n");
+            return;
+        }
 
-    var request_url = "http://localhost/inerd/public/sub_url.php?type="+type+"&url=("+url+")&finder="+finder+"&callback=?";
-    $.getJSON(request_url, function(data){
-      alert(data.state);
-      //$("#sub-url-finder").val(data.state);
+        if (finder == "") {
+            alert("\n\n请输入您的大名，让我们知道您 ！！！\n\n");
+            return;
+        }
+
+        var request_url = "http://onlylemi.com/inerd/public/sub_url.php?callback=?";
+        // alert(request_url);
+        $.getJSON(request_url, { "type": type, "finder": finder, "url": url }, function(data) {
+            if (data.state == "exist") {
+                alert("\n\n不好意思，您提交的资源链接已经存在了！！！\n\n");
+            } else if (data.state == "success") {
+                alert("\n\n非常感谢，您提交的资源我们已经收到！\n\n");
+            } else if (data.state == "none") {
+                alert("\n\n服务器正在维护中，请稍等！！！\n\n");
+            }
+        });
     });
-  });
+
+    function isURL(url) {
+        var regex = "((http|ftp|https)://)(([a-zA-Z0-9\\._-]+\\.[a-zA-Z]{2,6})|([0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}))(:[0-9]{1,4})*(/[a-zA-Z0-9\\&%_\\./-~-]*)?";
+        var re = new RegExp(regex);
+        if (re.test(url)) {
+            return true;
+        } 
+        return false;
+    }
 
 
 
